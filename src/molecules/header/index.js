16db31c893eb5f08dd81fd4ddcb3e95d49ca7f2e
Matt Travi <programmer@travi.org>
2017-08-24 00:07:21 -0500

    build(eslint): enabled eslint and fixed existing issues

--- src/molecules/header/index.js
+++ src/molecules/header/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {AppBar, Toolbar} from 'material-ui';
 
-export default function () {
+export default function Header() {
   return (
     <AppBar>
       <Toolbar />
     </AppBar>
   );
 }
+
+Header.displayName = 'Header';
