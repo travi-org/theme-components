6dad00be3be320b89e796c6eb06111d251a7b192
Matt Travi <programmer@travi.org>
2019-01-15 22:13:18 -0600

    feat(typography): used typography to configure text display

--- src/organisms/layout/presentational.js
+++ src/organisms/layout/presentational.js
@@ -9,7 +9,9 @@ export default function Layout({children}) {
     <MuiThemeProvider theme={theme}>
       <CssBaseline />
       <Header />
-      {children}
+      <section>
+        {children}
+      </section>
     </MuiThemeProvider>
   );
 }
