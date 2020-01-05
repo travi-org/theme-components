127ad941d82f25d757095fa519b584a61364fbaf
Matt Travi <programmer@travi.org>
2020-01-05 01:16:57 -0600

    feat(layout): exposed the component through the public interface

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,23 @@
 /* eslint import/no-extraneous-dependencies: ['error', {'devDependencies': true}] */
+import babel from 'rollup-plugin-babel';
 import autoExternal from 'rollup-plugin-auto-external';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
   input: 'src/index.js',
-  plugins: [autoExternal()],
+  plugins: [
+    autoExternal(),
+    nodeResolve({mainFields: ['module']}),
+    babel({
+      babelrc: false,
+      exclude: ['./node_modules/**'],
+      presets: [
+        ['@travi', {targets: {node: 10, browser: true}, react: true, modules: false}],
+        '@emotion/babel-preset-css-prop'
+      ],
+      plugins: [['transform-react-remove-prop-types', {mode: 'wrap'}]]
+    })
+  ],
   output: [
     {file: 'lib/index.cjs.js', format: 'cjs', sourcemap: true},
     {file: 'lib/index.es.js', format: 'es', sourcemap: true}
