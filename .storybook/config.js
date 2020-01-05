592574522b45e6ea93488cf016cebc7983fbbc77
Matt Travi <programmer@travi.org>
2020-01-05 01:36:30 -0600

    docs(stories): got storybook working for the added components

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,19 @@
 import {configure} from '@storybook/react';
+import {useMediaQuery} from '@material-ui/core';
+import {MuiThemeProvider} from '@material-ui/core/styles';
+import {addDecorator} from '@storybook/react';
+import createTheme from '../src/theme';
+
+const Wrapper = ({story}) => {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  return (
+    <MuiThemeProvider theme={createTheme(prefersDarkMode ? 'dark' : 'light')}>
+      {story()}
+    </MuiThemeProvider>
+  );
+};
+
+addDecorator(story => <Wrapper story={story} />);
 
 configure(require.context('../src', true, /stories\.js$/), module);
