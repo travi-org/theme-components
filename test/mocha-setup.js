a76ce3cbfcf927d6dc3a5a8a42acc7c241daaf26
Matt Travi <programmer@travi.org>
2020-01-05 00:59:20 -0600

    test: got the build passing after adding the theme components

--- test/mocha-setup.js
+++ test/mocha-setup.js
@@ -1,5 +1,12 @@
 import sinon from 'sinon';
 import chai from 'chai';
+import {configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter()});
+
+console.error = err => { throw new Error(err); };           // eslint-disable-line no-console
+console.warn = warning => { throw new Error(warning); };    // eslint-disable-line no-console
 
 sinon.assert.expose(chai.assert, {prefix: ''});
 
