393c6d52b3d286da6d0d497f31252544e7931687
Matt Travi <programmer@travi.org>
2020-03-12 21:19:33 -0500

    test(integration): handled the testing-library deprecation

--- test/integration/logo-spec.js
+++ test/integration/logo-spec.js
@@ -2,6 +2,6 @@ describe('Logo Test', () => {
   it('Renders correctly', () => {
     cy.visit('/iframe.html?id=atoms-logo--default');
 
-    cy.get('span').queryByText('Travi').should('exist');
+    cy.get('span').findByText('Travi').should('exist');
   });
 });
